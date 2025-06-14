1c86cc7429de10139a389f52139e239dd6d53dcb
Javi Sanz <pracker@gmail.com>
2025-06-14 20:54:51 +0200

    feat: last attempt for today eof line

--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -3,23 +3,27 @@ import globals from 'globals';
 import reactHooks from 'eslint-plugin-react-hooks';
 import reactRefresh from 'eslint-plugin-react-refresh';
 import tseslint from 'typescript-eslint';
+import eslintPluginPrettier from 'eslint-plugin-prettier';
+import eslintPluginEditorconfig from 'eslint-plugin-editorconfig';
 
 export default tseslint.config(
   { ignores: ['dist'] },
   {
     extends: [js.configs.recommended, ...tseslint.configs.recommended],
     files: ['**/*.{ts,tsx}'],
+    rules: {
+      'editorconfig/eol-last': 'error',
+      'prettier/prettier': 'error',
+    },
     languageOptions: {
       ecmaVersion: 2020,
       globals: globals.browser,
     },
     plugins: {
+      editorconfig: eslintPluginEditorconfig,
+      prettier: eslintPluginPrettier,
       'react-hooks': reactHooks,
       'react-refresh': reactRefresh,
-    },
-    rules: {
-      ...reactHooks.configs.recommended.rules,
-      'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
     },
   },
 );
