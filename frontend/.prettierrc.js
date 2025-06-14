5884b3a73d3472a8f01b60dc808847493a711d6c
Javi Sanz <pracker@gmail.com>
2025-06-14 19:14:19 +0200

    feat: try to add line EOF

---
+++ frontend/.prettierrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+  semi: true,
+  singleQuote: true,
+  trailingComma: 'all',
+  tabWidth: 2,
+  printWidth: 100,
+  endOfLine: 'lf',
+  insertFinalNewline: true,
+  requireConfig: false,
+};
